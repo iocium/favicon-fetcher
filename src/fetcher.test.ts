c4e6fddd3b8a35767b55934de437ee6c75a28c2f
Matthew Gall <me@matthewgall.dev>
2025-05-04 12:29:47 +0000

    Added more tests to increase coverage by mocking providers

--- src/fetcher.test.ts
+++ src/fetcher.test.ts
@@ -1,7 +1,31 @@
 import { FaviconFetcher } from './fetcher';
 
 describe('FaviconFetcher (mocked)', () => {
-  it('successfully fetches mocked favicon', async () => {
+  it('successfully fetches mocked favicon from google', async () => {
+    const fetcher = new FaviconFetcher('bbc.co.uk');
+    const result = await fetcher.fetchFavicon('google');
+
+    expect(result.status).toBe(200);
+    expect(result.contentType).toBe('image/png');
+    expect(result.content.byteLength).toBeGreaterThan(0);
+  });
+  it('successfully fetches mocked favicon from duckduckgo', async () => {
+    const fetcher = new FaviconFetcher('reddit.com');
+    const result = await fetcher.fetchFavicon('duckduckgo');
+
+    expect(result.status).toBe(200);
+    expect(result.contentType).toBe('image/png');
+    expect(result.content.byteLength).toBeGreaterThan(0);
+  });
+  it('successfully fetches mocked favicon from bitwarden', async () => {
+    const fetcher = new FaviconFetcher('facebook.com');
+    const result = await fetcher.fetchFavicon('bitwarden');
+
+    expect(result.status).toBe(200);
+    expect(result.contentType).toBe('image/png');
+    expect(result.content.byteLength).toBeGreaterThan(0);
+  });
+  it('successfully fetches mocked favicon from icon.horse', async () => {
     const fetcher = new FaviconFetcher('github.com');
     const result = await fetcher.fetchFavicon('iconHorse');
 
@@ -9,4 +33,5 @@ describe('FaviconFetcher (mocked)', () => {
     expect(result.contentType).toBe('image/png');
     expect(result.content.byteLength).toBeGreaterThan(0);
   });
+
 });
--- test/server.ts
+++ test/server.ts
@@ -2,10 +2,36 @@ import { setupServer } from 'msw/node';
 import { http } from 'msw';
 
 export const server = setupServer(
+  http.get('https://www.google.com/s2/favicons', ({ request }) => {
+    const url = new URL(request.url);
+    const domain = url.searchParams.get('domain')
+
+    if (!domain) {
+      return new Response(null, { status: 404 })
+    }
+
+    return new Response(new ArrayBuffer(10), {
+      status: 200,
+      headers: { 'Content-Type': 'image/png' }
+    });
+  }),
+  http.get('https://icons.duckduckgo.com/ip3/reddit.com.ico', ({ request }) => {
+    return new Response(new ArrayBuffer(10), {
+      status: 200,
+      headers: { 'Content-Type': 'image/png' }
+    });
+  }),
+  http.get('https://icons.bitwarden.net/facebook.com/icon.png', ({ request }) => {
+    return new Response(new ArrayBuffer(10), {
+      status: 200,
+      headers: { 'Content-Type': 'image/png' }
+    });
+  }),
   http.get('https://icon.horse/icon/github.com', () => {
     return new Response(new ArrayBuffer(10), {
       status: 200,
       headers: { 'Content-Type': 'image/png' }
     });
-  })
+  }),
+
 );
